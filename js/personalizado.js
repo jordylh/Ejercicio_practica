006e26a816656ecbc6be1336175a5b512be0fbe2
jordylh <jordylagunashiguera@gmail.com>
2025-06-13 05:10:56 -0600

    Seccion nueva y modificacion de menu

--- js/personalizado.js
+++ js/personalizado.js
@@ -1,4 +1,18 @@
 var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop:true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+var swiper2 = new Swiper(".mySwiper2",{
+    slidesPerView: "auto",
+    centeredSlides:true,
+    spaceBetween:400,  
+    loop:true,
     pagination: {
         el: ".swiper-pagination",
     },
